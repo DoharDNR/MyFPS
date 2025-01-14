1a0cb7f35698bdc29d69c261d9ae4ce38b28d7bb
DNRDev <cartjonnson@gmail.com>
2025-01-14 17:18:24 +0700

    Update Content and UI

--- app.js
+++ app.js
@@ -34,3 +34,22 @@ function scrollFunction(){
         }
     }
 }
+
+function openPage(pageName, elmnt, color){
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for(i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablink");
+    for(i = 0; i < tablinks.length; i++){
+        tabcontent[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(pageName).style.display = "block";
+
+    elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
